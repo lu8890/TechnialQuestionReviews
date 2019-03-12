f7d6c76d3ff3561f29ae9c32e883c6112aa67bd8
shihlu@hotmail.com <shihlu@hotmail.com>
2019-03-12 04:20:15 -0700

    add LeetCode Problem Solutions for 1. RemoveElement 2. StrStr, 3. SearchInsert, 4. CountAndSay 5. MaxSubArray

---
+++ TechReviews/LeetCode/Problems/EasyQuestions2.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lu8890.TechReviews.LeetCode.Problems
+{
+    public partial class EasyQuestions
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/remove-element/
+        /// Runtime: 252 ms, faster than 84.53% of C# online submissions for Remove Element.
+        /// Memory Usage: 28.5 MB, less than 5.06% of C# online submissions for Remove Element.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (!nums.Contains(val))
+                return nums.Length;
+            if (nums.Count(x => x != val) == nums.Length)
+                return 0;
+
+            var startIndex = 0;
+            var traverse = 0;
+            var swapInt = 0;
+
+            while (startIndex < nums.Length)
+            {
+                if (nums[startIndex] == val)
+                {
+                    traverse = startIndex + 1;
+                    while (traverse < nums.Length)
+                    {
+                        if (nums[traverse] == val)
+                            ++traverse;
+                        else
+                        {
+                            swapInt = nums[startIndex];
+                            nums[startIndex] = nums[traverse];
+                            nums[traverse] = swapInt;
+                            break;
+                        }
+                    }
+                }
+
+                ++startIndex;
+            }
+
+            return nums.Count(x => x != val);
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/implement-strstr/
+        /// Runtime: 72 ms, faster than 100.00% of C# online submissions for Implement strStr().
+        /// Memory Usage: 20.4 MB, less than 47.83% of C# online submissions for Implement strStr().
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needle"></param>
+        /// <returns></returns>
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrWhiteSpace(haystack) && string.IsNullOrEmpty(needle))
+                return 0;
+            if ((string.IsNullOrWhiteSpace(haystack)) || (needle.Length > haystack.Length))
+                return -1;
+            if (string.IsNullOrEmpty(needle))
+                return 0;
+
+            return haystack.IndexOf(needle, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/search-insert-position/
+        /// Runtime: 96 ms, faster than 85.26% of C# online submissions for Search Insert Position.
+        /// Memory Usage: 22.9 MB, less than 8.33% of C# online submissions for Search Insert Position.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert(int[] nums, int target)
+        {
+            if ((nums == null) || (nums.Length == 0))
+                return 1;
+
+            if (nums.Contains(target))
+            {
+                return nums.Length - nums.SkipWhile(x => x != target).Count();
+            }
+            else
+            { 
+                return nums.Length - nums.SkipWhile(x => x < target).Count();
+            }
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/count-and-say/
+        /// Runtime: 84 ms, faster than 94.73% of C# online submissions for Count and Say.
+        /// Memory Usage: 23.7 MB, less than 46.51% of C# online submissions for Count and Say.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public string CountAndSay(int n)
+        {
+            if (n <= 0)
+                return "0";
+            else if (n == 1)
+                return "1";
+            else if (n == 2)
+                return "11";
+            else
+            {
+                var CAS = "11";
+
+                for (var i = 2; i < n; i++)
+                {
+                    CAS = BuildCountAndSay(CAS);
+                }
+
+                return CAS;
+            }
+        }
+
+        private static string BuildCountAndSay(string input)
+        {
+            var inArray = input.ToCharArray();
+            var map = new List<StringBuilder>();
+
+            char key = inArray[0];
+            var token = new StringBuilder();
+            token.Append(key);
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (input[i] == key)
+                    token.Append(input[i]);
+                else
+                {
+                    map.Add(token);
+                    token = new StringBuilder();
+                    token.Append(input[i]);
+                    key = input[i];
+                }
+            }
+            if(!string.IsNullOrWhiteSpace(token.ToString()))
+                map.Add(token);
+
+            var resultBuilder = new StringBuilder();
+            foreach (var t in map)
+            {
+                var lineArray = t.ToString().ToCharArray();
+                resultBuilder.Append(lineArray.Length);
+                resultBuilder.Append(Convert.ToString(lineArray[0]));
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/maximum-subarray/
+        /// Time Limited Exceeded
+        /// Submission failed with Time Limited Exceeded after passed 200/202 test cases.
+        /// The failing test case has with nums.Length > 3000
+        ///
+        /// round 2:
+        /// Runtime: 400 ms, faster than 8.72% of C# online submissions for Maximum Subarray.
+        /// Memory Usage: 23.3 MB, less than 29.25% of C# online submissions for Maximum Subarray.
+        ///
+        /// round 3:
+        /// Runtime: 396 ms, faster than 9.01% of C# online submissions for Maximum Subarray.
+        /// Memory Usage: 23.4 MB, less than 8.49% of C# online submissions for Maximum Subarray.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MaxSubArray(int[] nums)
+        {
+            if ((nums == null) || (nums.Length == 0))
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+
+            var sum = nums[0];
+            var traverseIndex = 0;
+            int max = nums[0];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum = nums[i];
+                if (nums[i] > max)
+                    max = nums[i];
+
+                traverseIndex = i + 1;
+                while (traverseIndex < nums.Length)
+                {
+                    sum += nums[traverseIndex];
+                    if (sum > max)
+                        max = sum;
+                    ++traverseIndex;
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// This is the copy/modification of a C++ solution found in discussion section.
+        /// https://leetcode.com/problems/maximum-subarray/
+        /// Runtime: 104 ms, faster than 80.82% of C# online submissions for Maximum Subarray.
+        /// Memory Usage: 23.2 MB, less than 73.58% of C# online submissions for Maximum Subarray.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MaxSubArray2(int[] nums)
+        {
+            if ((nums == null) || (nums.Length == 0))
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+
+            var sum = nums[0];
+            var maxSum = sum;
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                sum = (nums[i] > sum + nums[i])
+                    ? nums[i]
+                    : sum + nums[i];
+
+                maxSum = (sum > maxSum)
+                    ? sum
+                    : maxSum;
+            }
+
+            return maxSum;
+        }
+
+        /// <summary>
+        /// from a Python solution in discussion
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MaxSubArray3(int[] nums)
+        {
+            if ((nums == null) || (nums.Length == 0))
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+
+            for (var i=1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] > 0)
+                    nums[i] += nums[i - 1];
+            }
+
+            return nums.Max(x => x);
+        }
+    }
+}
---
+++ TechReviewsTests/LeetCode/Problems/EasyQuestions2Tests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lu8890.TechReviews.LeetCode.Problems;
+
+
+namespace lu8890.TechReviewsTests.LeetCode.Problems
+{
+    [TestClass]
+    public class EasyQuestions2Tests
+    {
+        [TestMethod]
+        public void RemoveElementTest()
+        {
+            var testFunc = new EasyQuestions();
+            var testcase = new int[] {3, 2, 2, 3};
+            var output = testFunc.RemoveElement(testcase, 3);
+            Assert.AreEqual(2, output);
+            var result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] {2, 2});
+
+            testcase = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            output = testFunc.RemoveElement(testcase, 2);
+            Assert.AreEqual(5, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { 0, 1, 3, 0, 4 });
+
+            testcase = new int[] { 3, 2, 2, 3 };
+            output = testFunc.RemoveElement(testcase, 2);
+            Assert.AreEqual(2, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { 3,3 });
+
+            testcase = new int[] { };
+            output = testFunc.RemoveElement(testcase, 2);
+            Assert.AreEqual(0, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { });
+
+            testcase = new int[] { 1};
+            output = testFunc.RemoveElement(testcase, 2);
+            Assert.AreEqual(1, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { 1});
+
+            testcase = new int[] { 1 };
+            output = testFunc.RemoveElement(testcase, 1);
+            Assert.AreEqual(0, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { });
+
+            testcase = new int[] { 3,3,1,2,4,3,3 };
+            output = testFunc.RemoveElement(testcase, 3);
+            Assert.AreEqual(3, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] {1,2,4});
+
+            testcase = new int[] { 3, 3, 1, 2,3, 4, 3, 3 };
+            output = testFunc.RemoveElement(testcase, 3);
+            Assert.AreEqual(3, output);
+            result = GenerateResultIntOutput(testcase, output);
+            ValidateArrayElements(result, new int[] { 1, 2, 4 });
+        }
+
+        private static void ValidateArrayElements(int[] expected, int[] actual)
+        {
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (var i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+        }
+
+        private static int[] GenerateResultIntOutput(int[] input, int count)
+        {
+            if ((input == null) || (input.Length < count))
+                return null;
+
+            var output = new int[count];
+            for (var i = 0; i < count; i++)
+                output[i] = input[i];
+
+            return output;
+        }
+
+        [TestMethod]
+        public void StrStrTest()
+        {
+            var testFunc = new EasyQuestions();
+            Assert.AreEqual(-1, testFunc.StrStr(string.Empty, "ll"));
+            Assert.AreEqual(0, testFunc.StrStr("hello", string.Empty));
+            Assert.AreEqual(0, testFunc.StrStr(string.Empty, string.Empty));
+            Assert.AreEqual(2, testFunc.StrStr("hello","ll"));
+            Assert.AreEqual(-1, testFunc.StrStr("aaaaa", "bba"));
+            Assert.AreEqual(0, testFunc.StrStr("aaaaa", "aaa"));
+            Assert.AreEqual(1, testFunc.StrStr("baaaaa", "aaa"));
+        }
+
+        [TestMethod]
+        public void SearchInsertTest()
+        {
+            var testFunc = new EasyQuestions();
+            Assert.AreEqual(2, testFunc.SearchInsert(new int[] {1, 3, 5, 6}, 5));
+            Assert.AreEqual(1, testFunc.SearchInsert(new int[] {1, 3, 5, 6}, 2));
+            Assert.AreEqual(4, testFunc.SearchInsert(new int[] {1, 3, 5, 6}, 7));
+            Assert.AreEqual(0, testFunc.SearchInsert(new int[] {1, 3, 5, 6}, 0));
+
+        }
+
+        [TestMethod]
+        public void CountAndSayTest()
+        {
+            var testFunc = new EasyQuestions();
+            Assert.AreEqual("0", testFunc.CountAndSay(0));
+            Assert.AreEqual("1", testFunc.CountAndSay(1));
+            Assert.AreEqual("11", testFunc.CountAndSay(2));
+            Assert.AreEqual("21", testFunc.CountAndSay(3));
+            Assert.AreEqual("1211", testFunc.CountAndSay(4));
+            Assert.AreEqual("111221", testFunc.CountAndSay(5));
+            Assert.AreEqual("312211", testFunc.CountAndSay(6));
+            Assert.AreEqual("13112221", testFunc.CountAndSay(7));
+            Assert.AreEqual("1113213211", testFunc.CountAndSay(8));
+        }
+
+        [TestMethod]
+        public void MaxSubArrayTest()
+        {
+            var testFunc = new MaxSubArrayTestDel(new EasyQuestions().MaxSubArray);
+            RunMaxSubArrayTest(testFunc);
+        }
+
+        [TestMethod]
+        public void MaxSubArray2Test()
+        {
+            var testFunc = new MaxSubArrayTestDel(new EasyQuestions().MaxSubArray2);
+            RunMaxSubArrayTest(testFunc);
+        }
+
+        [TestMethod]
+        public void MaxSubArray3Test()
+        {
+            var testFunc = new MaxSubArrayTestDel(new EasyQuestions().MaxSubArray3);
+            RunMaxSubArrayTest(testFunc);
+        }
+
+        public delegate int MaxSubArrayTestDel(int[] nums);
+
+        private void RunMaxSubArrayTest(MaxSubArrayTestDel testFunc)
+        {
+            Assert.AreEqual(0, testFunc.Invoke(new int[] { }));
+            Assert.AreEqual(0, testFunc.Invoke(new int[] { 0 }));
+            Assert.AreEqual(-1, testFunc.Invoke(new int[] { -1 }));
+            Assert.AreEqual(10, testFunc.Invoke(new int[] { -1, 10 }));
+            Assert.AreEqual(5, testFunc.Invoke(new int[] { -1, 3, 2 }));
+            Assert.AreEqual(6, testFunc.Invoke(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+
+        }
+    }
+}
