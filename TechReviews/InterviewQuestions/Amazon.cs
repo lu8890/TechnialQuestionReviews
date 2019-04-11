17dd3486530c2eaaa618619e162720a88ee282a9
shihlu@hotmail.com <shihlu@hotmail.com>
2019-04-11 13:29:08 -0700

    Amazon interview question: HasATriange

--- TechReviews/InterviewQuestions/Amazon.cs
+++ TechReviews/InterviewQuestions/Amazon.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace lu8890.TechReviews.InterviewQuestions
 {
-    class Amazon
+    public class Amazon
     {
+        /// <summary>
+        /// Given an array of integers.  Return a boolean indicating if it contains lengths of a triangle
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <returns></returns>
+        public bool HasATriangle(int[] inputArray)
+        {
+            if (inputArray.Length < 3)
+                return false;
+            inputArray = inputArray.OrderBy(x => x).ToArray();
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                for (var j = i + 1; j < inputArray.Length; j++)
+                {
+                    for (var k = j + 1; k < inputArray.Length; k++)
+                    {
+                        if (inputArray[k] <= inputArray[i] + inputArray[j])
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
---
+++ TechReviewsTests/InterviewQuestions/Amazon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lu8890.TechReviews.InterviewQuestions;
+
+namespace lu8890.TechReviewsTests.InterviewQuestions
+{
+    [TestClass]
+    public class Amazon
+    {
+        [TestMethod]
+        public void HasATriangleTest()
+        {
+            var testClass = new TechReviews.InterviewQuestions.Amazon();
+            var output = testClass.HasATriangle(new int[] {1, 0, 2, 4, 3});
+            Assert.AreEqual(true, output);
+
+            output = testClass.HasATriangle(new int[] { });
+            Assert.AreEqual(false, output);
+
+            output = testClass.HasATriangle(new int[] {1});
+            Assert.AreEqual(false, output);
+
+            output = testClass.HasATriangle(new int[] {1, 2});
+            Assert.AreEqual(false, output);
+
+            output = testClass.HasATriangle(new int[] {3, 1, 2});
+            Assert.AreEqual(true, output);
+
+            output = testClass.HasATriangle(new int[] {3, 0, 1, 0, 2});
+            Assert.AreEqual(true, output);
+
+            output = testClass.HasATriangle(new int[] {0, 0, 3});
+            Assert.AreEqual(false, output);
+
+            output = testClass.HasATriangle(new int[] {1, 3, 1});
+            Assert.AreEqual(false, output);
+
+            output = testClass.HasATriangle(new int[] {1, 1, 1, 3, 1, 1, 0, 2});
+            Assert.AreEqual(true, output);
+
+            output = testClass.HasATriangle(new int[] {0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5});
+            Assert.AreEqual(true, output);
+        }
+    }
+}
