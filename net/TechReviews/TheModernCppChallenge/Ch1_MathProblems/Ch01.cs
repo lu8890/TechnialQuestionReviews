53b4c4595007886fa6b6e1f9f30ba15f593e9c30
tommylu_resume <tommylu_resume@hotmail.com>
2020-01-15 17:11:27 -0800

    New question solutions checkin

---
+++ net/TechReviews/TheModernCppChallenge/Ch1_MathProblems/Ch01.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lu8890.TechReviews.TheModernCppChallenge.Ch1_MathProblems
+{
+    public class Ch01
+    {
+        /// <summary>
+        /// 01. Sum of naturals divisible by 3 and 5
+        /// Write a program that calculates and prints the sum of all the natural numbers divisible 
+        /// by either 3 or 5, up to a given limit entered by the user
+        /// 
+        /// Note: Natural numbers == positibe integers
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public int SumOfNaturalNumsDivisibleBy3And5(int limit)
+        {
+            if (limit > int.MaxValue)
+                throw new ArgumentOutOfRangeException();
+
+            if (limit == 0)
+                return 0;
+
+            var ResultSum = 0;
+
+            for(int i=1; i <= limit; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    Console.WriteLine($"Target Number Found: {i}");
+                    ResultSum += i;
+                }
+            }
+
+            return ResultSum;
+        }
+
+        /// <summary>
+        /// 02. Greatest common divisor
+        /// Write a program that, given two positive integers, will calculate and print the greatest common
+        /// divisor of the two
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int FindGreatestCommonDivisor(int x, int y)
+        {
+            if((x > int.MaxValue) || (y > int.MaxValue))
+                throw new ArgumentOutOfRangeException();
+
+            if ((x == 0) || (y == 0))
+                throw new ArgumentOutOfRangeException();
+
+            var SmallOfTheTwo = (x <= y)
+                ? Math.Abs(x)
+                : Math.Abs(y);
+
+            var ResultCol = new List<int>();
+
+            for(int i=1; i<= SmallOfTheTwo; i++)
+            {
+                if (((x % i) == 0) && ((y % i) == 0))
+                    ResultCol.Add(i);
+            }
+
+            ResultCol = ResultCol.OrderByDescending(arg=>arg).ToList();
+
+            return ResultCol.Any()
+                    ? ResultCol.FirstOrDefault()
+                    : -1;
+        }
+
+        /// <summary>
+        /// 03. Least common multiple
+        /// Write a program that will, given two or more positive integers, clculate and print the 
+        /// least common multiple of them all.
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        public int FindLeastCommonMultiple(List<int> inputs)
+        {
+            
+            if ((inputs == null) || (!inputs.Any()))
+                return 0;
+
+            if (inputs.Any(x => x <= 0))
+                throw new ArgumentOutOfRangeException();
+
+            if (inputs.Any(x => x > int.MaxValue))
+                throw new ArgumentOutOfRangeException();
+
+            if (inputs.Count == 1)
+                return 1;
+
+            var counter = 2;
+            var MatchingCounter = 0;
+
+            while (true)
+            {
+                inputs.ForEach(x =>
+                { 
+                    if((counter % x) == 0)
+                    {
+                        ++MatchingCounter;
+                    }
+                });
+
+                if (MatchingCounter == inputs.Count)
+                    return counter;
+
+                MatchingCounter = 0;
+                ++counter;
+            }
+        }
+
+        /// <summary>
+        /// 04. Largest prime smaller than given number
+        /// Write a program that computes and prints the largest prime number that is smaller than a number 
+        /// provided by the user, which must be a positive integer
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public int? LargestPrimeNumber(int limit)
+        {
+            if ((limit > int.MaxValue) || (limit < 0))
+                throw new ArgumentOutOfRangeException();
+
+            if (limit <= 1)
+                return null;
+
+            if (limit <= 3)
+                return limit;
+
+            var PrimeCounter = limit;
+
+            while(PrimeCounter > 0)
+            {
+                if (IsPrime(PrimeCounter))
+                    return PrimeCounter;
+
+                --PrimeCounter;
+            }
+
+            throw new Exception("No Solution Found");
+        }
+
+        private bool IsPrime(int input)
+        {
+            var counter = 1;
+
+            for(int i=input; i >1; i--)
+            {
+                if ((input % i) == 0)
+                    ++counter;
+            }
+
+            return (counter == 2)
+                    ? true
+                    : false;
+        }
+
+        /// <summary>
+        /// 05. Sexy prime pairs
+        /// Write a program that prints all the sexy prime pairs up to a limit entered by
+        /// the user.
+        /// 
+        /// Note: sexy prime pair = two prime numbers that diff by 6
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public Dictionary<int, int> GetSexyPrimePairs(int limit)
+        {
+
+            if ((limit > int.MaxValue) || (limit < 0))
+                throw new ArgumentOutOfRangeException();
+
+            var result = new Dictionary<int, int>();
+
+            for(int i=2; i <=limit; i++)
+            {
+                if ((i + 6) <= limit)
+                {
+                    if (IsPrime(i) && IsPrime(i + 6))
+                        result.Add(i, i + 6);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ net/TechReviewsTests/TheModernCppChallenge/Ch1_MathProblems/Ch01Tests.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lu8890.TechReviews.TheModernCppChallenge.Ch1_MathProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lu8890.TechReviews.TheModernCppChallenge.Ch1_MathProblems.Tests
+{
+    [TestClass()]
+    public class Ch01Tests
+    {
+        [TestMethod()]
+        public void SumOfNaturalNumsDivisibleBy3And5Test()
+        {
+            var TestRunner = GetCh01Instance();
+            var result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(0);
+            Assert.AreEqual(0, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(1);
+            Assert.AreEqual(0, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(2);
+            Assert.AreEqual(0, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(3);
+            Assert.AreEqual(3, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(4);
+            Assert.AreEqual(3, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(5);
+            Assert.AreEqual(8, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(6);
+            Assert.AreEqual(14, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(7);
+            Assert.AreEqual(14, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(8);
+            Assert.AreEqual(14, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(9);
+            Assert.AreEqual(23, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(10);
+            Assert.AreEqual(33, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(11);
+            Assert.AreEqual(33, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(12);
+            Assert.AreEqual(45, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(13);
+            Assert.AreEqual(45, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(14);
+            Assert.AreEqual(45, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(15);
+            Assert.AreEqual(60, result);
+            result = TestRunner.SumOfNaturalNumsDivisibleBy3And5(16);
+            Assert.AreEqual(60, result);
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindGreatestCommonDivisorExceptionTest()
+        {
+            var TestRunner = GetCh01Instance();
+            TestRunner.FindGreatestCommonDivisor(1,0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindGreatestCommonDivisorException2Test()
+        {
+            var TestRunner = GetCh01Instance();
+            TestRunner.FindGreatestCommonDivisor(0, 1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindGreatestCommonDivisorException3Test()
+        {
+            var TestRunner = GetCh01Instance();
+            TestRunner.FindGreatestCommonDivisor(0, 0);
+        }
+
+        [TestMethod()]
+        public void FindGreatestCommonDivisorTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.FindGreatestCommonDivisor(1, 1);
+            Assert.AreEqual(1, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(2, 1);
+            Assert.AreEqual(1, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(1, 2);
+            Assert.AreEqual(1, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(2, 2);
+            Assert.AreEqual(2, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(2, 4);
+            Assert.AreEqual(2, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(4, 2);
+            Assert.AreEqual(2, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(16, 256);
+            Assert.AreEqual(16, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(256, 16);
+            Assert.AreEqual(16, Result);
+            Result = TestRunner.FindGreatestCommonDivisor(2, -2);
+            Assert.AreEqual(2, Result);
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindLeastCommonMultipleExceptionTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 2, 0 });
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindLeastCommonMultipleException2Test()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.FindLeastCommonMultiple(new List<int>() { -1, 0, 13 });
+        }
+
+        [TestMethod()]
+        public void FindLeastCommonMultipleTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 2, 2 });
+            Assert.AreEqual(2, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 12, 24 });
+            Assert.AreEqual(24, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 3, 7 });
+            Assert.AreEqual(21, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 2, 5 });
+            Assert.AreEqual(10, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 3, 13 });
+            Assert.AreEqual(39, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 9, 24 });
+            Assert.AreEqual(72, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>());
+            Assert.AreEqual(0, Result);
+            Result = TestRunner.FindLeastCommonMultiple(null);
+            Assert.AreEqual(0, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() {3, 8, 24 });
+            Assert.AreEqual(24, Result);
+            Result = TestRunner.FindLeastCommonMultiple(new List<int>() { 2, 3, 6 });
+            Assert.AreEqual(6, Result);
+        }
+
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void LargestPrimeNumberExceptionTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.LargestPrimeNumber(-1);
+        }
+
+        [TestMethod()]
+        public void LargestPrimeNumberTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.LargestPrimeNumber(0);
+            Assert.AreEqual(null, Result);
+            Result = TestRunner.LargestPrimeNumber(1);
+            Assert.AreEqual(null, Result);
+            Result = TestRunner.LargestPrimeNumber(2);
+            Assert.AreEqual(2, Result);
+            Result = TestRunner.LargestPrimeNumber(3);
+            Assert.AreEqual(3, Result);
+            Result = TestRunner.LargestPrimeNumber(4);
+            Assert.AreEqual(3, Result);
+            Result = TestRunner.LargestPrimeNumber(5);
+            Assert.AreEqual(5, Result);
+            Result = TestRunner.LargestPrimeNumber(6);
+            Assert.AreEqual(5, Result);
+            Result = TestRunner.LargestPrimeNumber(7);
+            Assert.AreEqual(7, Result);
+            Result = TestRunner.LargestPrimeNumber(8);
+            Assert.AreEqual(7, Result);
+            Result = TestRunner.LargestPrimeNumber(9);
+            Assert.AreEqual(7, Result);
+            Result = TestRunner.LargestPrimeNumber(10);
+            Assert.AreEqual(7, Result);
+            Result = TestRunner.LargestPrimeNumber(11);
+            Assert.AreEqual(11, Result);
+            Result = TestRunner.LargestPrimeNumber(12);
+            Assert.AreEqual(11, Result);
+            Result = TestRunner.LargestPrimeNumber(13);
+            Assert.AreEqual(13, Result);
+            Result = TestRunner.LargestPrimeNumber(14);
+            Assert.AreEqual(13, Result);
+            Result = TestRunner.LargestPrimeNumber(15);
+            Assert.AreEqual(13, Result);
+            Result = TestRunner.LargestPrimeNumber(16);
+            Assert.AreEqual(13, Result);
+            Result = TestRunner.LargestPrimeNumber(17);
+            Assert.AreEqual(17, Result);
+            Result = TestRunner.LargestPrimeNumber(18);
+            Assert.AreEqual(17, Result);
+            Result = TestRunner.LargestPrimeNumber(19);
+            Assert.AreEqual(19, Result);
+            Result = TestRunner.LargestPrimeNumber(20);
+            Assert.AreEqual(19, Result);
+            Result = TestRunner.LargestPrimeNumber(199);
+            Assert.AreEqual(199, Result);
+            Result = TestRunner.LargestPrimeNumber(200);
+            Assert.AreEqual(199, Result);
+            Result = TestRunner.LargestPrimeNumber(195);
+            Assert.AreEqual(193, Result);
+
+        }
+
+
+        [TestMethod()]
+        public void GetSexyPrimePairsTest()
+        {
+            var TestRunner = GetCh01Instance();
+            var Result = TestRunner.GetSexyPrimePairs(3);
+            CollectionAssert.AreEqual(new Dictionary<int, int>(), Result);
+            Result = TestRunner.GetSexyPrimePairs(11);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11} }, Result);
+            Result = TestRunner.GetSexyPrimePairs(12);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11 } }, Result);
+            Result = TestRunner.GetSexyPrimePairs(13);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11 }, {7, 13 } }, Result);
+            Result = TestRunner.GetSexyPrimePairs(14);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11 }, { 7, 13 } }, Result);
+            Result = TestRunner.GetSexyPrimePairs(17);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11 }, { 7, 13 }, { 11, 17 } }, Result);
+            Result = TestRunner.GetSexyPrimePairs(19);
+            CollectionAssert.AreEqual(new Dictionary<int, int>() { { 5, 11 }, { 7, 13 }, { 11, 17 }, { 13, 19 } }, Result);
+        }
+
+        private Ch01 GetCh01Instance()
+        {
+            return new Ch01();
+        }
+    }
+}
